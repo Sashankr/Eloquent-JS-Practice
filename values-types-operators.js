db382b0fd1d39f676f52b0d0af20cea33a97d19f
Sashankr <sashankr97@gmail.com>
2026-04-13 08:47:53 +0530

    values, operators, strings

--- values-types-operators.js
+++ values-types-operators.js
@@ -9,7 +9,13 @@ console.log('String',b);
 
 // Template literal
 console.log(`100 divided by 2 is ${100/2}`);
-
+// Strings are also a sequence of bits in memory, however, each character
+of the string has been assigned a value, this is called the Unicode representa-
+tion, Strings are represented in 16 bits, and 2 power 16 combinations can be
+represented using the strings.
+This however poses challenges when using emoji's, because each emoji, takes up
+two bits.
 // Unary operator
 console.log(typeof 10);
 console.log(-a);
+
