8e5e0db260e4cebfc557fbc2bf2827a6593dab1e
Sashankr <sashankr97@gmail.com>
2026-04-14 08:01:30 +0530

    program structure

---
+++ program-structure.js
@@ -0,0 +1,15 @@
+// An expression is a fragment of a program.
+// A statement is an expression with a semicolon at the end;
+// A program is a list of statements.
+
+// In order to keep a reference of a value or the result of an expression, we can store that in a variable, also called binding.
+// Think of binding as a tentacle instead of a box, we are grabbing the value and binding it with a tentacle.
+let a = 10;
+
+// the value of this variable can be changed.
+a = 20;
+console.log(a); // 20
+
+
+
+
