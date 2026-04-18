39ec99fa90d8ef389f2c9215df04aac3427703d8
Sashankr <sashankr97@gmail.com>
2026-04-18 15:13:01 +0530

    FizzBuzz

--- program-structure.js
+++ program-structure.js
@@ -101,3 +101,60 @@ switch(currentItem){
     break;
 }
 console.log(currentItem,price);
+
+// Exercises.
+
+/*
+ * Print this
+#
+##
+###
+####
+#####
+######
+####### 
+*/
+
+let str=""
+for(let i=0;i<6;i++){
+  str +='#'
+  console.log(`${str}\n`);
+  
+
+}
+
+// Fizzbuzz
+/*
+Write a program that uses console.log to print all the numbers from 1
+to 100, with two exceptions. For numbers divisible by 3, print "Fizz"
+instead of the number, and for numbers divisible by 5 (and not 3), print
+"Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz
+" for numbers that are divisible by both 3 and 5 (and still print "Fizz"
+or "Buzz" for numbers divisible by only one of those).
+(This is actually an interview question that has been claimed to weed
+out a significant percentage of programmer candidates. So if you solved
+it, your labor market value just went up.)
+
+Pseudo code
+loop for 100
+current num % 3 === 0, return Fizz
+current num % 3 !== 0 && current num % 5 === 0, return Buzz
+
+current num % 3 === 0 && current num % 5 === 0
+return FizzBuzz
+*/
+
+for(let i=1;i<=100;i++){
+ if(i%3===0 && i%5===0){
+   console.log("FizzBuzz");
+  } else if (i%3 ===0){
+    console.log("Fizz")
+  } else if(i%5===0){
+    console.log("Buzz");
+  } else{
+    console.log(i);
+  }
+}
+
+
