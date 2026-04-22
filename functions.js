8ee31591946768a85c16aff8eaa87c1f4b22433c
Sashankr <sashankr97@gmail.com>
2026-04-22 09:25:10 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,12 @@
+// Functions
+// A binding that stores a part of program, that can be used by invoking it.
+// It helps us organize code, reduce repitition.
+
+let square = function(num){
+ return num * num;
+}
+
+square(2); //4
+// Each function starts with a keyword function followed by parantheses.
+// The comma separated bindings inside the parantheses are called parameters.
+// When the function is called we can pass arguements to these parameters.
